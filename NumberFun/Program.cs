0ffe84fb7f0adb7c5ba42ae9d348693225077a39
Paddy Branson <paddy.branson@education.nsw.gov.au>
2017-10-18 09:41:27 +1100

    NumberFun

---
+++ NumberFun/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number1 ");
+            float Num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number2");
+            float Num2 = float.Parse(Console.ReadLine());
+
+                //Output
+            Console.WriteLine("Number 1 plus Number 2 =" + (Num1 + Num2));
+            Console.WriteLine("Number 1 Times Number 2 =" + (Num1 * Num2));
+            Console.WriteLine("Number 1 Minus Number 2 =" + (Num1 - Num2));
+            Console.WriteLine("Number 1 Divided By Number 2 =" + (Num1 / Num2));
+
+                //Wait At End
+            Console.ReadLine();
+        }
+    }
+}
