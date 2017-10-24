76ad4c2ad1580bb1d03c41935b502d46716d41bc
Paddy Branson <paddy.branson@education.nsw.gov.au>
2017-10-25 10:21:42 +1100

    GuessMyNumber

---
+++ GuessMyNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessMyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Generate Random Number
+            Random rnd = new System.Random();
+            int RandomNumber = rnd.Next(1, 101);
+
+            //Set Guess Number To Out Of Guess Range
+            int guess = -1;
+
+            //Start Loop
+            while (guess != RandomNumber)
+            {
+                //Input
+                guess = int.Parse(Console.ReadLine());
+
+                //Selection
+                if (guess > RandomNumber)
+                {
+                    Console.WriteLine("Too High");
+                }
+                else if (guess < RandomNumber)
+                {
+                    Console.WriteLine("Too Low");
+                }
+                else 
+                {
+                    Console.WriteLine("Correct");
+                }
+
+            }
+            //Wait
+            Console.ReadKey();
+        }   
+    }
+}
