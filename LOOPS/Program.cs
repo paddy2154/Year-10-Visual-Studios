532f8dc6bb40d5a2dd23b6aed93392abfc732a31
Paddy Branson <paddy.branson@education.nsw.gov.au>
2017-10-24 14:58:42 +1100

    LOOPS

---
+++ LOOPS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter To Start...");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            int Number = int.Parse(Console.ReadLine());
+
+            for (int Num = 1; Num <= Number; Num++)
+            {
+                Console.Write(Num);
+            }
+
+            // WAIT AT END
+            Console.ReadLine();
+        }
+    }
+}
